8144f9ae51b4f44afd613c73e717535567d94da0
RZahr <dev-mobile@geovision.com.lb>
2021-01-25 16:57:01 +0200

    initial

--- extensions/src/main/java/com/rzahr/extensions/Extensions.kt
+++ extensions/src/main/java/com/rzahr/extensions/Extensions.kt
@@ -2,10 +2,14 @@ package com.rzahr.extensions
 
 import android.app.ActivityManager
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.database.Cursor
+import android.os.BatteryManager
 import android.os.Build
 import android.util.Log
 import java.util.*
+import kotlin.math.roundToInt
 
 /**
  * gets the device name
@@ -50,3 +54,24 @@ fun Class<*>.isMyServiceRunning(context: Context): Boolean {
 }
 
 fun Cursor.valueOf(columnName: String) = if (this.getColumnIndex(columnName) != -1) this.getString(this.getColumnIndex(columnName)) else null
+
+/**
+ * gets the battery level
+ * @return the battery level
+ */
+fun getBatteryLevel(context: Context): Int {
+
+    return try {
+
+        val batteryIntent = context
+            .registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        val batteryLevel = batteryIntent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        val batteryScale = batteryIntent?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+
+        if (batteryLevel == -1 || batteryScale == -1) 50f.roundToInt() else (batteryLevel.toFloat() / batteryScale.toFloat() * 100f).roundToInt()
+
+    } catch (exc: Exception) {
+
+        0
+    }
+}
